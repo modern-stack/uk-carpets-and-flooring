dd988f95a557344b90bb2b5e9981d4c6b2f8779c
dackers86 <ackers86@hotmail.com>
2019-02-11 20:24:09 +0000

    latest

--- src/components/contentful/Carousel/index.js
+++ src/components/contentful/Carousel/index.js
@@ -1,9 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Image from 'gatsby-image'
-import { CarouselContainer, Slide } from './styled'
+import {
+  CarouselContainer,
+  Slide,
+  SlideSelection,
+  SlideSelector,
+} from './styled'
+
+const incrementCarousel = (position, setPosition, slideCount) => {
+  setPosition(position + 1 > slideCount - 1 ? 0 : position + 1)
+}
 
 export default ({ slides }) => {
   const [position, setPosition] = useState(0)
+
+  useEffect($ => {
+    const carouselTimer = setTimeout(
+      () => incrementCarousel(position, setPosition, slides.length),
+      10000
+    )
+    return () => {
+      clearTimeout(carouselTimer)
+    }
+  })
 
   return (
     <div>
@@ -11,16 +30,28 @@ export default ({ slides }) => {
         {slides.map((slide, index) => (
           <Slide key={index} visible={index === position}>
             <Image style={{ width: '100%' }} fluid={slide.image.fluid} />
+            <h1>We Provide Ultimate Flooring</h1>
+            <h2>
+              We make it a priority to offer flexible servuce to accomodate your
+              needs.
+            </h2>
+
+            <button />
           </Slide>
         ))}
+
+        <SlideSelection>
+          {slides.map(($, i) => (
+            <SlideSelector
+              onClick={() => setPosition(i)}
+              selected={i === position}
+            />
+          ))}
+        </SlideSelection>
       </CarouselContainer>
       <button
-        onClick={() =>
-          setPosition(position + 1 > slides.length - 1 ? 0 : position + 1)
-        }
-      >
-        Next
-      </button>
+        onClick={() => incrementCarousel(position, setPosition, slides.length)}
+      />
     </div>
   )
 }
--- src/components/contentful/Carousel/styled.js
+++ src/components/contentful/Carousel/styled.js
@@ -1,13 +1,8 @@
 import styled from 'styled-components'
 
 const CarouselContainer = styled.div`
-  max-width: 100%;
-`
-
-const Selectors = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+  position: relative;
+  height: 100%;
 `
 
 const Slide = styled.div`
@@ -15,7 +10,27 @@ const Slide = styled.div`
   overflow: hidden;
   height: ${props => (props.visible ? '80vh' : 0)};
   opacity: ${props => (props.visible ? 1 : 0)};
-  transition: opacity 1s ease-in-out;
+  transition: opacity 1s ease-in;
 `
 
-export { CarouselContainer, Slide, Selectors }
+const SlideSelection = styled.div`
+  display: flex;
+  position: absolute;
+  bottom: 10%;
+  width: 100%;
+  justify-content: center;
+`
+
+const SlideSelector = styled.div`
+  width: 30px;
+  height: 5px;
+  margin: 0 4px;
+  background-color: ${props =>
+    props.selected ? 'rgb(199, 0, 43, 1)' : 'white'};
+
+  cursor: ${$ => ($.selected ? 'cursor' : 'pointer')};
+
+  transition: color 1s ease-in;
+`
+
+export { CarouselContainer, Slide, SlideSelection, SlideSelector }
