a7eb7eb829699158412140dc7989fddde84ceb27
Darren Ackers <darren.ackers@checkd.media>
2019-02-18 13:08:23 +0000

    Responsive Insta feed on mobile

--- src/components/instagram/styled.js
+++ src/components/instagram/styled.js
@@ -11,6 +11,11 @@ const Gallery = styled.div`
   grid-template-columns: repeat(12, 1fr);
   margin: 0 auto;
   padding: 8px;
+
+  @media (max-width: 700px) {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+  }
 `
 
 const Title = styled.div`
