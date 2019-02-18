778af1cdcd3b6e1927707531be6a0c0075632284
dackers86 <ackers86@hotmail.com>
2019-02-18 21:27:27 +0000

    another bug fix

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
-import '../../styles/global'
 
 import Header from '../header'
 import Menu from '../menu'
