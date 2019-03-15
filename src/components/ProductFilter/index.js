6f2c247372e4e4854a98934d005d0bf450609907
Darren Ackers <darren.ackers@checkd.media>
2019-03-15 18:07:43 +0000

    Stated filter

--- src/components/ProductFilter/index.js
+++ src/components/ProductFilter/index.js
@@ -1,11 +1,64 @@
-import React from 'react'
-import { ProductFilter, Title, SubTitle, Selection } from './styled'
+import React, { useState } from 'react'
+import {
+  ProductFilter,
+  Title,
+  SubTitle,
+  Selection,
+  Selected,
+  Option,
+  OptionHeader,
+} from './styled'
 
 export default () => (
   <ProductFilter>
     <Title>Filter By</Title>
     <Selection>
-      <li>Somthing</li>
+      <OptionHeader>
+        <div>Color</div>
+        <div>Dash</div>
+      </OptionHeader>
+      <Option>
+        <Selected selected /> Light(17)
+      </Option>
+      <Option>
+        <Selected /> Light(17)
+      </Option>
+      <Option>
+        <Selected /> Light(17)
+      </Option>
+      <Option>
+        <Selected /> Light(17)
+      </Option>
+      <Option>
+        <Selected /> Light(17)
+      </Option>
+    </Selection>
+    <Selection>
+      <Option>
+        <Selected />
+      </Option>
+    </Selection>
+
+    <Selection>
+      <OptionHeader>
+        <div>traditional Woods</div>
+        <div>Dash</div>
+      </OptionHeader>
+      <Option>
+        <Selected /> Light(17)
+      </Option>
+      <Option>
+        <Selected /> Light(17)
+      </Option>
+      <Option>
+        <Selected /> Light(17)
+      </Option>
+      <Option>
+        <Selected /> Light(17)
+      </Option>
+      <Option>
+        <Selected /> Light(17)
+      </Option>
     </Selection>
   </ProductFilter>
 )
--- src/components/ProductFilter/styled.js
+++ src/components/ProductFilter/styled.js
@@ -1,18 +1,62 @@
 import styled from 'styled-components'
 
-const ProductFilter = styled.div``
+const ProductFilter = styled.div`
+  padding: 0px 20px;
+  margin-right: 20px;
+`
 
 const Title = styled.div``
 
 const SubTitle = styled.div``
 
-const CheckBox = styled.div`
-  background-color: greenl;
+const Selected = styled.div`
+  width: 8px;
+  height: 8px;
+  position: relative;
+  background: white;
+  overflow: hidden;
+  padding: 3px;
+  border: 1px solid black;
+  margin-right: 20px;
+
+  :after {
+    content: '';
+    background: ${$ => ($.selected ? 'black' : 'white')};
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    right: 2px;
+    bottom: 2px;
+  }
 `
 
-const Selection = styled.ul`
-  list-style: none;
+const Selection = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px 0px 20px 0px;
+  border-bottom: 1px solid grey;
+
+  & > div {
+    padding: 10px 0;
+  }
+`
+
+const Option = styled.div`
+  display: flex;
+`
+
+const OptionHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  text-transform: uppercase;
+`
+
+export {
+  ProductFilter,
+  Title,
+  SubTitle,
+  Selection,
+  Selected,
+  OptionHeader,
+  Option,
 }
-`
-
-export { ProductFilter, Title, SubTitle, Selection }
