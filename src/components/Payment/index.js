49cc40840106414209441a93630961b6e88dee18
dackers86 <ackers86@hotmail.com>
2019-04-23 22:01:04 +0100

    removred stripe

--- src/components/Payment/index.js
+++ src/components/Payment/index.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { Payment } from './styled'
 
 import { Elements, StripeProvider } from 'react-stripe-elements'
-import CheckoutForm from './CheckoutForm'
+import CheckoutForm from './checkoutForm'
 
 export default () => (
   <Payment>
+    <div>Payment form</div>
     {/* <StripeProvider apiKey="pk_test_j8D2dhgBhWY1ToEZm9NsrF48">
       <div className="example">
         <h1>React Stripe Elements Examples</h1>
