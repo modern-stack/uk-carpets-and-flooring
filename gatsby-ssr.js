05ee472379e69bc672948d0afd19ad5b8fe61ef9
Darren Ackers <darren.ackers@checkd.media>
2019-04-25 16:35:54 +0100

    Context ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,1 +1,16 @@
 import React from 'react'
+import { Context } from './src/Context'
+
+export const replaceRenderer = ({
+  bodyComponent,
+  replaceBodyHTMLString,
+  setHeadComponents,
+}) => {
+  const App = () => {
+    return <Context>{bodyComponent}</Context>
+  }
+
+  const body = renderToString(<App />)
+
+  replaceBodyHTMLString(body)
+}
