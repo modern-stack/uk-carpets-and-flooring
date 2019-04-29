5af7da05021199dea7672a7d253c49927dff0d95
dackers86 <ackers86@hotmail.com>
2019-04-29 20:19:51 +0100

    test contentful components

--- src/templates/home.js
+++ src/templates/home.js
@@ -1,23 +1,23 @@
 import React from 'react'
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+// import Layout from '../components/layout'
+// import SEO from '../components/seo'
 // import ContentfulComponents from '../components/contentful'
 
 // import Testimonials from '../components/testimonials'
 
 export default ({ pageContext }) => {
   return (
-    <Layout page={pageContext}>
-      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-      <React.Fragment>
-        {/* {pageContext.contentfulPage.content
+    // <Layout page={pageContext}>
+    // <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <React.Fragment>
+      {/* {pageContext.contentfulPage.content
           .filter($ => $.internal)
           .map($ => {
             const Component = ContentfulComponents[$.internal.type]
             return Component ? <Component {...$} /> : null
           })} */}
-        {/* <Testimonials /> */}
-      </React.Fragment>
-    </Layout>
+      {/* <Testimonials /> */}
+    </React.Fragment>
+    // </Layout>
   )
 }
