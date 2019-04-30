fd8d7c11ae07c94fcbefec898f5394eabf28cbcd
dackers86 <ackers86@hotmail.com>
2019-04-30 18:37:29 +0100

    ssr firebase

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -1,23 +1,23 @@
-import { initializeApp, auth } from 'firebase'
+// import { initializeApp, auth } from 'firebase'
 
-const config = {
-  apiKey: process.env.GATSBY_FIREBASE_API_KEY,
-  authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.GATSBY_FIREBASE_DATABSE_URL,
-  projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
-}
+// const config = {
+//   apiKey: process.env.GATSBY_FIREBASE_API_KEY,
+//   authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.GATSBY_FIREBASE_DATABSE_URL,
+//   projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+// }
 
-class Firebase {
-  constructor() {
-    if (typeof window !== 'undefined') {
-      initializeApp(config)
-      this.auth = auth()
+// class Firebase {
+//   constructor() {
+//     if (typeof window !== 'undefined') {
+//       initializeApp(config)
+//       this.auth = auth()
 
-      this.SignIn = () => {
-        this.auth.signInWithPopup(new auth.FacebookAuthProvider())
-      }
-    }
-  }
-}
+//       this.SignIn = () => {
+//         this.auth.signInWithPopup(new auth.FacebookAuthProvider())
+//       }
+//     }
+//   }
+// }
 
-export default new Firebase()
+// export default new Firebase()
