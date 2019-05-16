10e479689eeb519d7446c399e724bb563da2fbcc
Darren Ackers <darren.ackers@checkd.media>
2019-05-16 12:32:41 +0100

    Updated callback url

--- src/services/Auth/index.js
+++ src/services/Auth/index.js
@@ -4,7 +4,7 @@ import { navigateTo } from 'gatsby-link'
 const auth = new auth0.WebAuth({
   domain: process.env.GATSBY_AUTH0_DOMAIN,
   clientID: process.env.GATSBY_AUTH0_CLIENT_ID,
-  redirectUri: 'http://localhost:8000/callback',
+  redirectUri: process.env.GATSBY_AUTH0_CALLBACK_URL,
   audience: `https://${process.env.GATSBY_AUTH0_DOMAIN}/api/v2/`,
   responseType: 'token id_token',
   scope: 'openid profile email app_metadata',
