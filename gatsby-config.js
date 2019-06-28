c5a0cafcf55792d1f36cd5f97841944e65c69b0f
Darren Ackers <darren.ackers@checkd.media>
2019-06-28 13:42:49 +0100

    Added offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,9 +61,10 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
+    'gatsby-plugin-offline',
     'gatsby-plugin-netlify',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
