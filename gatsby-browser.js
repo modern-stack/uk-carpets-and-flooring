6a5fd1ea968002729064af3630fc09380306b141
dackers86 <ackers86@hotmail.com>
2019-06-30 12:07:02 +0100

    test without stripe

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,12 +7,21 @@ import { StripeProvider } from 'react-stripe-elements'
 
 import client from './src/services/Apollo'
 
+// var script = document.createElement('script')
+// script.type = 'text/javascript'
+// script.src = 'https://js.stripe.com/v3/'
+// document.body.appendChild(script)
+// script.onload = function() {
+//   Stripe.setPublishableKey('pk_test_j8D2dhgBhWY1ToEZm9NsrF48')
+//   // do stuff
+// }
+
 export const wrapRootElement = ({ element }) => {
   return (
     <ApolloProvider client={client}>
-      <StripeProvider apiKey="pk_test_j8D2dhgBhWY1ToEZm9NsrF48">
-        {element}
-      </StripeProvider>
+      {/* <StripeProvider apiKey="pk_test_j8D2dhgBhWY1ToEZm9NsrF48"> */}
+      {element}
+      {/* </StripeProvider> */}
     </ApolloProvider>
   )
 }
