7856a9f2305867b04d1198f81ee4b2bc5d353009
dackers86 <ackers86@hotmail.com>
2019-07-18 21:49:48 +0100

    remove mobile carousel page test

--- src/templates/home.js
+++ src/templates/home.js
@@ -1,21 +1,18 @@
 import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import PrismicComponents from '../components/Prismic'
-
-import Testimonials from '../components/testimonials'
 
 export default ({ pageContext }) => {
   return (
     <Layout page={pageContext}>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-      <React.Fragment>
+      {/* <React.Fragment>
         {pageContext.prismicPage.data.body.map($ => {
           const Component = PrismicComponents[$.slice_type]
           return Component ? <Component {...$} /> : null
         })}
         <Testimonials />
-      </React.Fragment>
+      </React.Fragment> */}
     </Layout>
   )
 }
