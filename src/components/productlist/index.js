14790b259afcc3be3a88993e1eca4c24060034ce
dackers86 <ackers86@hotmail.com>
2019-07-29 19:15:19 +0100

    Image not found template added

--- src/components/productlist/index.js
+++ src/components/productlist/index.js
@@ -2,8 +2,21 @@ import React from 'react'
 import Image from 'gatsby-image'
 import { List, Product, ImageContainer } from './styled'
 import { navigateTo } from 'gatsby-link'
+import { useStaticQuery, graphql } from 'gatsby'
 
 export default ({ products }) => {
+  const localQuery = useStaticQuery(graphql`
+    query {
+      ImageNotFound: file(relativePath: { eq: "image-not-found.jpeg" }) {
+        childImageSharp {
+          fluid(quality: 100, maxHeight: 960, maxWidth: 1280) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <List>
       {products.map($ => {
@@ -20,8 +33,10 @@ export default ({ products }) => {
                 )
               }
             >
-              {featured_image && featured_image.localFile && (
+              {featured_image && featured_image.localFile ? (
                 <Image fluid={featured_image.localFile.childImageSharp.fluid} />
+              ) : (
+                <Image fluid={localQuery.ImageNotFound.childImageSharp.fluid} />
               )}
             </ImageContainer>
 
