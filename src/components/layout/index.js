57b57b2a2d1bf6e2f44153ed068335619f64bf1c
dackers86 <ackers86@hotmail.com>
2019-10-18 20:55:33 +0100

    Loadable footer

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,9 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
-import Footer from '../footer'
 import Copyright from '../copyright'
-
-import GlobalStyle from './globalstyle'
 
 import { Page, Content } from './styled'
 import { ThemeProvider } from 'styled-components'
@@ -34,6 +31,11 @@ const LoadableFooter = Loadable({
 
 const LoadableStyle = Loadable({
   loader: () => import('./globalstyle'),
+  loading: () => <div>loading...</div>,
+})
+
+const LoadableFooter = Loadable({
+  loader: () => import('../footer'),
   loading: () => <div>loading...</div>,
 })
 
