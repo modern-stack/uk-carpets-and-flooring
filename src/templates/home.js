69283e3eedd8b0c05a6f2a71c677873c622ccf99
Darren Ackers <darren.ackers@checkd.media>
2019-10-18 13:23:31 +0100

    Loadable test

--- src/templates/home.js
+++ src/templates/home.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import Loadable from 'react-loadable'
-import Layout from '../components/layout'
-import SEO from '../components/seo'
 
-import Carousel from '../components/Prismic/Carousel'
-import Banners from '../components/Prismic/PrismicBanners'
-import Testimonials from '../components/testimonials'
+import SEO from '../components/seo'
 
 const LoadableComponent = Loadable({
   loader: () => import('../components/layout'),
+  loading: () => <div>loading...</div>,
+})
+
+const LoadableCarousel = Loadable({
+  loader: () => import('../components/Prismic/Carousel'),
+  loading: () => <div>loading...</div>,
+})
+
+const LoadableBanners = Loadable({
+  loader: () => import('../components/Prismic/PrismicBanners'),
+  loading: () => <div>loading...</div>,
+})
+
+const LoadableTestimonials = Loadable({
+  loader: () => import('../components/testimonials'),
   loading: () => <div>loading...</div>,
 })
 
@@ -18,9 +29,9 @@ export default ({ pageContext }) => {
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
 
       <React.Fragment>
-        <Carousel items={pageContext.prismicPage.data.body[0].items} />
-        <Banners items={pageContext.prismicPage.data.body[1].items} />
-        <Testimonials />
+        <LoadableCarousel items={pageContext.prismicPage.data.body[0].items} />
+        <LoadableBanners items={pageContext.prismicPage.data.body[1].items} />
+        <LoadableTestimonials />
       </React.Fragment>
     </LoadableComponent>
   )
