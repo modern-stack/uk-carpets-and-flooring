525fe542b38ca7f768588399c2aa8109695beb9d
dackers86 <ackers86@hotmail.com>
2019-10-28 22:56:12 +0000

    wrapper test

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,12 @@
-// export { wrapRootElement } from './src/services/Apollo/wrap-root-element'
+export { wrapRootElement } from './src/services/Apollo/wrap-root-element'
 
-import './src/styles/global.css'
+// import './src/styles/global.css'
 
-import React from 'react'
-import { ApolloProvider } from 'react-apollo-hooks'
+// import React from 'react'
+// import { ApolloProvider } from 'react-apollo-hooks'
 
-import client from './src/services/Apollo'
+// import client from './src/services/Apollo'
 
-export const wrapRootElement = ({ element }) => {
-  return <ApolloProvider client={client}>{element}</ApolloProvider>
-}
+// export const wrapRootElement = ({ element }) => {
+//   return <ApolloProvider client={client}>{element}</ApolloProvider>
+// }
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,15 +1,15 @@
-//export { wrapRootElement } from './src/services/Apollo/wrap-root-element'
+export { wrapRootElement } from './src/services/Apollo/wrap-root-element'
 
-import React from 'react'
-import { ApolloProvider } from 'react-apollo-hooks'
-const { renderToString } = require('react-dom/server')
+// import React from 'react'
+// import { ApolloProvider } from 'react-apollo-hooks'
+// const { renderToString } = require('react-dom/server')
 
-import client from './src/services/Apollo'
+// import client from './src/services/Apollo'
 
-export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
-  const ConnectedBody = () => (
-    <ApolloProvider client={client}>{bodyComponent}</ApolloProvider>
-  )
+// export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
+//   const ConnectedBody = () => (
+//     <ApolloProvider client={client}>{bodyComponent}</ApolloProvider>
+//   )
 
-  replaceBodyHTMLString(renderToString(<ConnectedBody />))
-}
+//   replaceBodyHTMLString(renderToString(<ConnectedBody />))
+// }
