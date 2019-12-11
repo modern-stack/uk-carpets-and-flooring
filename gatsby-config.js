64526a3768886efc3b4beb716e64922f63298efa
dackers86 <ackers86@hotmail.com>
2019-12-11 21:49:38 +0000

    Added push notifications

--- gatsby-config.js
+++ gatsby-config.js
@@ -95,7 +95,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-offline',
       options: {
-        appendScript: require.resolve(`src/static/sw-extension.js`),
+        appendScript: require.resolve(`./src/static/sw-extension.js`),
       },
     },
     'gatsby-plugin-netlify',
